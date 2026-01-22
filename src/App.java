850739ec8c11e5171878fe3503eb45ab02b77039
syedumerahmedcode <syed.umer.ahmed.code@gmail.com>
2026-01-22 08:47:19 +0100

    Feature: implemented the solution and tested it against a valid test case.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Given an array of integers heights representing the histogram's bar height
+         * where the width of each bar is 1, return the area of the largest rectangle in
+         * the histogram.
+         * 
+         * 
+         * 
+         * Example 1:
+         * 
+         * Input: heights = [2,1,5,6,2,3]
+         * Output: 10
+         * Explanation: The above is a histogram where width of each bar is 1.
+         * The largest rectangle is shown in the red area, which has an area = 10 units.
+         * 
+         * Example 2:
+         * 
+         * Input: heights = [2,4]
+         * Output: 4
+         * 
+         */
+        int[] heights = { 2, 1, 5, 6, 2, 3 };
+        Solution solution = new Solution();
+        int result = solution.largestRectangleArea(heights);
+        System.out.println("The given values are: " + Arrays.toString(heights) + " and the result is: " + result);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,6 +1,51 @@
+import java.util.Stack;
+
 public class Solution {
 
     public int largestRectangleArea(int[] heights) {
+        /**
+         * Initializing variables
+         */
+        int maxArea = 0;
+        /**
+         * Here we store indecies of the height
+         */
+        Stack<Integer> stack = new Stack<>();
+        /**
+         * number of items in the histogram (length of the given array).
+         */
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+            /**
+             * i==n, this is the condition when we arrive at the last element, the put
+             * height as 0 else take the current height
+             */
+            int currentHeight = (i == n) ? 0 : heights[i];
+            /**
+             * Check that currentHeight is less than the height value of the top of stack.
+             * If this is the case, then we have to calculate the area.
+             */
+            while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
+                /**
+                 * height on the top of stack(index value store in the stack which is used to
+                 * lookup the value of height in histogram)
+                 */
+                int height = heights[stack.pop()];
+                /**
+                 * i - stack.peek() - 1::: i=> currentHeight index
+                 * stack.peek()=> What is on tp of the stack
+                 * -1=>because arrays are 0 index based.
+                 */
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                /**
+                 * height * width=>This is the current area
+                 */
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
 
     }
 
